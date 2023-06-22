6cc1a685f48db6022d15eb6da8fb7dea76c6bf90
Huynh Duc Khoan <huynhduckhoan@gmail.com>
2023-06-22 14:29:43 +0700

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   branchPrefix: 'test-renovate/',
-  username: 'renovate-release',
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  username: 'bot',
+  gitAuthor: 'Renovate Bot <bot@go2js.com>',
   onboarding: false,
   endpoint: 'https://git.go2js.com/api/v1/',
   platform: 'gitea',
   includeForks: true,
   dryRun: 'full',
-  repositories: ['f97/go2js.com''],
+  repositories: ['f97/go2js.com'],
   packageRules: [
     {
       description: 'lockFileMaintenance',
