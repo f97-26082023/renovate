f26b76e77bd97f2c7b1c1c75c4b7a7af98f42507
Huynh Duc Khoan <huynhduckhoan@gmail.com>
2023-06-22 15:19:18 +0700

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -3,7 +3,10 @@ module.exports = {
   gitAuthor: 'Renovate Bot <bot@go2js.com>',
   endpoint: 'https://git.go2js.com/api/v1/',
   platform: 'gitea',
-  repositories: ['f97/go2js.com'],
+  repositories: [
+    'f97/go2js.com',
+    'phoenix/phoenix'
+  ],
   packageRules: [
     {
       dependencyDashboardApproval: false,
